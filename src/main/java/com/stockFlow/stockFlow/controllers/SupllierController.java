343321a97650fe99160f8249b7fe9cb4ef318d8e
JoaoTorpe <victotorpe10@gmail.com>
2023-09-06 14:44:25 -0300

    Endpont for getting the total quantity of the suppliers

--- src/main/java/com/stockFlow/stockFlow/controllers/SupllierController.java
+++ src/main/java/com/stockFlow/stockFlow/controllers/SupllierController.java
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.stockFlow.stockFlow.DTOs.ProductDTO;
-import com.stockFlow.stockFlow.entities.Product;
 import com.stockFlow.stockFlow.entities.Supllier;
 import com.stockFlow.stockFlow.services.SupplierService;
 
@@ -24,44 +23,50 @@ import com.stockFlow.stockFlow.services.SupplierService;
 public class SupllierController {
 	@Autowired
 	SupplierService service;
-	 @CrossOrigin(origins = "*", allowedHeaders = "*")
+	@CrossOrigin(origins = "*", allowedHeaders = "*")
 	@GetMapping
 	public ResponseEntity<List<Supllier>> findAll(){
 		List<Supllier> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
-	 @CrossOrigin(origins = "*", allowedHeaders = "*")
+	@CrossOrigin(origins = "*", allowedHeaders = "*")
 	@GetMapping(value = "/{id}")
 	public Supllier findByid(@PathVariable Long id) {
 		return service.findById(id);
 	}
-	 @CrossOrigin(origins = "*", allowedHeaders = "*")
+	@CrossOrigin(origins = "*", allowedHeaders = "*")
 	@PostMapping
 	public void addSupllier(@RequestBody Supllier supllier) {
 		service.addSupllier(supllier);
 	}
-	 @CrossOrigin(origins = "*", allowedHeaders = "*")
+	@CrossOrigin(origins = "*", allowedHeaders = "*")
 	@GetMapping(value="/products/{id}")
 	public ResponseEntity<List<ProductDTO>> addSupllier(@PathVariable Long id) {
 		return  ResponseEntity.ok().body(service.getProduts(id));
 	}
-	
-	
-	 @CrossOrigin(origins = "*", allowedHeaders = "*")
+
+	@CrossOrigin(origins = "*", allowedHeaders = "*")
+	@GetMapping(value="/quant")
+	public ResponseEntity<Integer> addSupllier() {
+		return  ResponseEntity.ok().body(service.getTotalQuantity());
+	}
+
+
+	@CrossOrigin(origins = "*", allowedHeaders = "*")
 	@DeleteMapping(value="/{id}")
 	public void remove(@PathVariable Long id) {
-		
+
 		service.remove(id);
 	}
-	 
-	 @CrossOrigin(origins = "*", allowedHeaders = "*")
-		@PutMapping(value="/{id}")
-	 public void updateSpullier(@PathVariable Long id , @RequestBody Supllier newSupllier ) {
-		 
+
+	@CrossOrigin(origins = "*", allowedHeaders = "*")
+	@PutMapping(value="/{id}")
+	public void updateSpullier(@PathVariable Long id , @RequestBody Supllier newSupllier ) {
+
 		service.updateSupplier(id, newSupllier);
-		 
-	 }
-	
-	
-	
+
+	}
+
+
+
 }
